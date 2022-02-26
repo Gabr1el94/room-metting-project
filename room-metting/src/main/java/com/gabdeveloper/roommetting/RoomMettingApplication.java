95aaf6276376f8ade8be0012c0a937a7486bb313
Gabr1el94 <bielsystem64@gmail.com>
2022-02-26 18:16:04 -0300

    config initial application

---
+++ room-metting/src/main/java/com/gabdeveloper/roommetting/RoomMettingApplication.java
@@ -0,0 +1,13 @@
+package com.gabdeveloper.roommetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomMettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomMettingApplication.class, args);
+	}
+
+}
